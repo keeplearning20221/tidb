c8f8be5c639213f78bd7050c95305fe5ea17426a
aidan liu <aidan.liu@pingcap.com>
2023-01-31 16:53:37 +0800

    modified:   procedure_test.go 	modified:   ../planner/core/expression_rewriter.go

--- planner/core/expression_rewriter.go
+++ planner/core/expression_rewriter.go
@@ -1290,6 +1290,10 @@ func (er *expressionRewriter) Leave(originInNode ast.Node) (retNode ast.Node, ok
 		er.err = errors.Errorf("UnknownType: %T", v)
 		return retNode, false
 	}
+
+	if er.err != nil {
+		return retNode, false
+	}
 	return originInNode, true
 }
 

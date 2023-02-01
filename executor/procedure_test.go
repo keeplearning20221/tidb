ea2d37027468879a60d04a13c7211f21c0a243c4
aidan liu <aidan.liu@pingcap.com>
2023-02-01 15:51:12 +0800

    modified:   executor/procedure.go 	modified:   executor/procedure_test.go 	modified:   parser/parser.go 	modified:   parser/parser.y

--- executor/procedure_test.go
+++ executor/procedure_test.go
@@ -15,6 +15,7 @@
 package executor_test
 
 import (
+	"fmt"
 	"strconv"
 	"testing"
 
@@ -92,14 +93,14 @@ func TestCreateShowDropProcedure(t *testing.T) {
 			"select id,subject,score,row_number() over (partition by subject order by score desc) as rownum from user_score) as a inner join user_score as b on a.id=b.id where a.rownum<=10 order by a.subject ;" +
 			"select *,u.username,ua.address,CONCAT(u.username, \"-\" ,ua.address) as userinfo,avg(us.score) over (order by us.id rows 2 preceding) as current_avg,sum(score) over (order by us.id rows 2 preceding) as current_sum from user_score us" +
 			" left join user u on u.id = us.user_id left join user_address ua on ua.id = us.user_id;" +
-			"select a.id,a.username,a.password,a.age,a.sex from user a where a.id in (select user_id from user_score where score > 90);	end;",
+			"select a.id,a.username,a.password,a.age,a.sex from user a where a.id in (select user_id from user_score where score > 90);    end;",
 		"create procedure proc_12() begin select us.user_id,u.username,us.subject,us.score from user_score us left join user u on u.id = us.user_id where us.score > 90 group by us.user_id,us.subject,us.score;" +
 			"select us.user_id,u.username,us.subject,us.score from user_score us join user u on u.id = us.user_id where us.score > 90 group by us.user_id,us.subject,us.score;" +
 			"select a.id,a.username,a.password,a.age,a.sex,ad.address,CONCAT(a.username, \"-\" ,ad.address) as userinfo from user a left join user_address ad on a.id = ad.user_id where a.id > 10 and a.id < 50;" +
 			"select a.id,a.username,a.password,a.age,a.sex,ad.score from user a right join user_score ad on a.id = ad.user_id where a.id > 10 and a.id < 50;" +
 			"select a.id,a.username,a.password,a.age,a.sex,ad.score from user a left join user_score ad on a.id = ad.user_id where a.id in (select user_id from user_score where score > 90 and score < 99 ) " +
 			"union select a.id,a.username,a.password,a.age,a.sex,ad.score from user a left join user_score ad on a.id = ad.user_id where a.id in (select user_id from user_score where score > 30 and score < 70 ); end;",
-		// 	block recursion
+		//     block recursion
 		"create procedure proc_13() begin select @a; begin select @b; end; end",
 		"create procedure proc_14() begin select @a; insert into t2 select * from t1; begin select @b;update t2 set id = 1; end; end",
 		// test declared variable type
@@ -134,7 +135,7 @@ func TestCreateShowDropProcedure(t *testing.T) {
 			"select id,subject,score,row_number() over (partition by subject order by score desc) as rownum from user_score) as a inner join user_score as b on a.id=b.id where a.rownum<=10 order by a.subject ;" +
 			"select *,u.username,ua.address,CONCAT(u.username, \"-\" ,ua.address) as userinfo,avg(us.score) over (order by us.id rows 2 preceding) as current_avg,sum(score) over (order by us.id rows 2 preceding) as current_sum from user_score us" +
 			" left join user u on u.id = us.user_id left join user_address ua on ua.id = us.user_id;" +
-			"select a.id,a.username,a.password,a.age,a.sex from user a where a.id in (select user_id from user_score where score > 90);	end",
+			"select a.id,a.username,a.password,a.age,a.sex from user a where a.id in (select user_id from user_score where score > 90);    end",
 		" CREATE PROCEDURE `proc_12`() begin select us.user_id,u.username,us.subject,us.score from user_score us left join user u on u.id = us.user_id where us.score > 90 group by us.user_id,us.subject,us.score;" +
 			"select us.user_id,u.username,us.subject,us.score from user_score us join user u on u.id = us.user_id where us.score > 90 group by us.user_id,us.subject,us.score;" +
 			"select a.id,a.username,a.password,a.age,a.sex,ad.address,CONCAT(a.username, \"-\" ,ad.address) as userinfo from user a left join user_address ad on a.id = ad.user_id where a.id > 10 and a.id < 50;" +
@@ -239,4 +240,94 @@ func TestBaseCall(t *testing.T) {
 	tk.MustQuery("select * from t2 order by id").Check(testkit.Rows("1", "6", "7"))
 	tk.MustQuery("show create procedure t8").Check(testkit.Rows("t8 ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION " +
 		" CREATE PROCEDURE `t8`() insert into t2 select * from t1 utf8mb4 utf8mb4_bin utf8mb4_bin"))
+
+}
+
+func TestCallSelect(t *testing.T) {
+	store := testkit.CreateMockStore(t)
+	tk := testkit.NewTestKit(t, store)
+	tk.MustExec("use test")
+	tk.MustExec("CREATE TABLE `user` ( `id` int(11) NOT NULL, `username` VARCHAR(30) DEFAULT NULL, `password` VARCHAR(30) DEFAULT NULL, " +
+		"`age` int(11) NOT NULL, `sex` int(11) NOT NULL, PRIMARY KEY (`id`), KEY `username` (`username`) ) ENGINE=InnoDB;")
+	tk.MustExec("CREATE TABLE `user_score` ( `id` int(11) NOT NULL, `subject` int(11) NOT NULL, `user_id` int(11) NOT NULL, " +
+		"`score` int(11) NOT NULL, PRIMARY KEY (`id`) ) ENGINE=InnoDB;")
+	tk.MustExec("CREATE TABLE `user_address` ( `id` int(11) NOT NULL, `user_id` int(11) NOT NULL, `address` VARCHAR(30) DEFAULT NULL, " +
+		"PRIMARY KEY (`id`), KEY `address` (`address`) ) ENGINE=InnoDB;")
+	tk.InProcedure()
+	tk.MustExec(`create procedure insert_data(i int, s_i int)  begin insert into user values(i, CONCAT("username-", i),CONCAT("password-", i),FLOOR( 15 + RAND() * 23),Mod(i,2));
+    insert into user_score values(s_i, 1, i, FLOOR( 40 + i * 100));
+    set s_i=s_i+1;
+    insert into user_score values(s_i, 2, i, FLOOR( 40 + i * 100));
+    set s_i=s_i+1;
+    insert into user_score values(s_i, 3, i, FLOOR( 40 + i * 100));
+    set s_i=s_i+1;
+    insert into user_score values(s_i, 4, i, FLOOR( 40 + i * 100));
+    set s_i=s_i+1;
+    insert into user_score values(s_i, 5, i, FLOOR( 40 + i * 100));
+    set s_i=s_i+1;
+    insert into user_address values(i, i, CONCAT("useraddress-", i));
+    set i=i+1;  end;`)
+	for i := 1; i <= 100; i = i + 5 {
+		sql := fmt.Sprintf("call insert_data(%d,%d)", i, i)
+		tk.MustExec(sql)
+	}
+	tk.MustExec(`create procedure sp_select() begin 
+    select a.id,a.username,a.password,a.age,a.sex from user a where a.id > 10 and a.id < 50;
+
+    select us.subject,count(us.user_id),sum(us.score),avg(us.score),max(us.score),min(us.score) from user_score us
+        where us.score > 90 group by us.subject;
+
+    select *,rank() over (partition by subject order by score desc) as ranking from user_score;
+
+    select us.*,sum(us.score) over (order by us.id) as current_sum,
+       avg(us.score) over (order by us.id) as current_avg,
+       count(us.score) over (order by us.id) as current_count,
+       max(us.score) over (order by us.id) as current_max,
+       min(us.score) over (order by us.id) as current_min from user_score us;
+
+    select us.*,sum(us.score) over (order by us.id) as current_sum,
+       avg(us.score) over (order by us.id) as current_avg,
+       count(us.score) over (order by us.id) as current_count,
+       max(us.score) over (order by us.id) as current_max,
+       min(us.score) over (order by us.id) as current_min,
+       u.username ,ua.address,CONCAT(u.username, "-" ,ua.address) as userinfo
+       from user_score us left join user u on u.id = us.user_id left join user_address ua on ua.id = us.user_id;
+
+    SELECT DISTINCT us.user_id,u.username ,ua.address,CONCAT(u.username, "-" ,ua.address) as userinfo,
+        sum(us.score) from user_score us left join user u on u.id = us.user_id
+        left join user_address ua on ua.id = us.user_id group by us.user_id,u.username;
+
+    select a.subject,a.id,a.user_id,u.username, a.score,a.rownum from ( select id,user_id,subject,score,row_number() over (order by score desc) as rownum from user_score) as a
+        left join user u on a.user_id = u.id inner join user_score as b on a.id=b.id where a.rownum<=10 order by a.rownum ;
+
+    select a.subject,a.id,a.score,a.rownum from (
+        select id,subject,score,row_number() over (partition by subject order by score desc) as rownum from user_score) as a
+        inner join user_score as b on a.id=b.id where a.rownum<=10 order by a.subject ;
+    select *,u.username,ua.address,CONCAT(u.username, "-" ,ua.address) as userinfo,
+        avg(us.score) over (order by us.id rows 2 preceding) as current_avg,
+        sum(score) over (order by us.id rows 2 preceding) as current_sum from user_score us
+        left join user u on u.id = us.user_id left join user_address ua on ua.id = us.user_id;
+     
+    select a.id,a.username,a.password,a.age,a.sex from user a where a.id in (select user_id from user_score where score > 90);
+     
+    select us.user_id,u.username,us.subject,us.score from user_score us left join user u on u.id = us.user_id
+        where us.score > 90 group by us.user_id,us.subject,us.score;
+     
+    select us.user_id,u.username,us.subject,us.score from user_score us join user u on u.id = us.user_id
+         where us.score > 90 group by us.user_id,us.subject,us.score;
+     
+    select a.id,a.username,a.password,a.age,a.sex,ad.address,CONCAT(a.username, "-" ,ad.address) as userinfo from user a
+         left join user_address ad on a.id = ad.user_id where a.id > 10 and a.id < 50;
+
+    select a.id,a.username,a.password,a.age,a.sex,ad.score from user a right join user_score ad on a.id = ad.user_id
+        where a.id > 10 and a.id < 50;
+
+    select a.id,a.username,a.password,a.age,a.sex,ad.score from user a
+        left join user_score ad on a.id = ad.user_id where a.id in (select user_id from user_score where score > 90 and score < 99 )
+        union select a.id,a.username,a.password,a.age,a.sex,ad.score from user a
+        left join user_score ad on a.id = ad.user_id
+        where a.id in (select user_id from user_score where score > 30 and score < 70 );
+    end;
+    `)
+	tk.MustExec(`call sp_select`)
 }
